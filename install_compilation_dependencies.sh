3568d38198471de7e88b36672f4bbccb4321fda0
Rosca Alex <roscaalex19@gmail.com>
2022-05-24 15:18:10 +0200

    Use Ubuntu 22.04 in GitHub Actions workflow

--- install_compilation_dependencies.sh
+++ install_compilation_dependencies.sh
@@ -2,30 +2,20 @@
 
 printf "\n* Installing dependencies *\n\n"
 
-sudo pacman -S --needed "$@" \
-  base-devel \
-  clang \
+sudo apt install -y \
+  ninja-build \
+  libgtk-3-dev \
   cmake \
-  git \
-  git-lfs \
-  gtk3 \
-  libdrm \
-  libinput \
-  libxcb \
-  libxkbcommon \
-  meson \
-  opengl-driver \
-  pixman \
-  seatd \
-  udev \
-  unzip \
-  wayland \
-  wayland-protocols \
-  wlroots \
-  xcb-util-renderutil \
-  xcb-util-wm \
-  xorg-xwayland \
-  zip
+  clang \
+  libwlroots-dev \
+  libgl-dev \
+  libegl-dev \
+  libgles-dev \
+  libepoxy-dev \
+  libwayland-dev \
+  libxkbcommon-dev \
+  libpixman-1-dev \
+  libinput-dev
 
 printf "\n* Downloading the Flutter engine shared libraries *\n\n"
 
@@ -37,13 +27,15 @@ curl -L https://github.com/sony/flutter-embedded-linux/releases/download/"$engin
 
 printf "\n* Extracting the Flutter engine libraries *\n\n"
 
-unzip -o /tmp/elinux-x64-debug.zip -d /tmp
+mkdir deps
+
+unzip -o /tmp/elinux-x64-debug.zip -d /tmp || exit
 mv /tmp/libflutter_engine.so deps/libflutter_engine_debug.so
 
-unzip -o /tmp/elinux-x64-profile.zip -d /tmp
+unzip -o /tmp/elinux-x64-profile.zip -d /tmp || exit
 mv /tmp/libflutter_engine.so deps/libflutter_engine_profile.so
 
-unzip -o /tmp/elinux-x64-release.zip -d /tmp
+unzip -o /tmp/elinux-x64-release.zip -d /tmp || exit
 mv /tmp/libflutter_engine.so deps/libflutter_engine_release.so
 
 printf "\n* Done *\n\n"
